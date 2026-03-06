0f5b6ec374720fa00b6fbc22f1152e3b2ae0bc9a
tajo48 <tajo48@proton.me>
2026-03-06 16:08:16 +0100

    Initial commit

---
+++ Source/LabyrinthProtocol/LabyrinthProtocolGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LabyrinthProtocolGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALabyrinthProtocolGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALabyrinthProtocolGameMode();
+};
+
+
+
