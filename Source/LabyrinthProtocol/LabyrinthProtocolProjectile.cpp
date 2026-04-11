097c0c682239dee622d26a60edf4e3f40f94f580
Nikodem Decewicz <Niko41255@gmail.com>
2026-04-11 14:57:44 +0200

    collision

--- Source/LabyrinthProtocol/LabyrinthProtocolProjectile.cpp
+++ Source/LabyrinthProtocol/LabyrinthProtocolProjectile.cpp
@@ -4,23 +4,23 @@
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Components/SphereComponent.h"
 
-ALabyrinthProtocolProjectile::ALabyrinthProtocolProjectile() 
+ALabyrinthProtocolProjectile::ALabyrinthProtocolProjectile()
 {
 	// Use a sphere as a simple collision representation
-	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
-	CollisionComp->InitSphereRadius(5.0f);
-	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
-	CollisionComp->OnComponentHit.AddDynamic(this, &ALabyrinthProtocolProjectile::OnHit);		// set up a notification for when this component hits something blocking
+	CollisionComp = CreateDefaultSubobject< USphereComponent >( TEXT( "SphereComp" ) );
+	CollisionComp->InitSphereRadius( 5.0f );
+	CollisionComp->BodyInstance.SetCollisionProfileName( "Projectile" );
+	CollisionComp->OnComponentHit.AddDynamic( this, &ALabyrinthProtocolProjectile::OnHit ); // set up a notification for when this component hits something blocking
 
 	// Players can't walk on it
-	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
+	CollisionComp->SetWalkableSlopeOverride( FWalkableSlopeOverride( WalkableSlope_Unwalkable, 0.f ) );
 	CollisionComp->CanCharacterStepUpOn = ECB_No;
 
 	// Set as root component
 	RootComponent = CollisionComp;
 
 	// Use a ProjectileMovementComponent to govern this projectile's movement
-	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
+	ProjectileMovement = CreateDefaultSubobject< UProjectileMovementComponent >( TEXT( "ProjectileComp" ) );
 	ProjectileMovement->UpdatedComponent = CollisionComp;
 	ProjectileMovement->InitialSpeed = 3000.f;
 	ProjectileMovement->MaxSpeed = 3000.f;
@@ -31,12 +31,12 @@ ALabyrinthProtocolProjectile::ALabyrinthProtocolProjectile()
 	InitialLifeSpan = 3.0f;
 }
 
-void ALabyrinthProtocolProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+void ALabyrinthProtocolProjectile::OnHit( UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit )
 {
 	// Only add impulse and destroy projectile if we hit a physics
-	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
+	if( ( OtherActor != nullptr ) && ( OtherActor != this ) && ( OtherComp != nullptr ) && OtherComp->IsSimulatingPhysics() )
 	{
-		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
+		OtherComp->AddImpulseAtLocation( GetVelocity() * 100.0f, GetActorLocation() );
 
 		Destroy();
 	}
